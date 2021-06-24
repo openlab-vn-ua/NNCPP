e5066304ef7268dd7e10c61d531b5618f43ce647
Serge Ageyev <justboss777@gmail.com>
2021-06-24 14:25:24 +0300

    Adjusted PRNG init

--- src/nnjs.cpp
+++ src/nnjs.cpp
@@ -9,4 +9,4 @@
 bool NN::DIV_IN_TRAIN = false; 
 
 //extern 
-Random NN::Internal::PRNG(); // PRNG = new Random(new Date().getTime());
+Random NN::Internal::PRNG; // PRNG = new Random(new Date().getTime());
